46bd756f1c5a9ce41fd118fc81e0fb975928a2ac
DijhonsDeveloper <dijhonsdeveloper1023@gmail.com>
2022-06-18 02:15:49 -0500

    Home Section Ok

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1  + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+
